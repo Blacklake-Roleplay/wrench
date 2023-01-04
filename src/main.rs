71e978dbfa2792c49598976ef59f463a02cde7bb
Vamist <dm@vamist.dev>
2023-01-04 23:11:46 +0000

    [new] clap, changelog and more
    - Changelog is now shown on discord embds
    
    - Clap is used for arg handling and documentation
    
    - user_agent is now required for discord support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 extern crate pest;
 #[macro_use]
-
 extern crate pest_derive;
 mod acf;
 
 use acf::parser;
 use acf::parser::AcfValue;
 
+use clap::Parser as ClapParser;
 use rand::{thread_rng, Rng};
+use rayon::prelude::*;
+use reqwest::header::USER_AGENT;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::{env, fs};
 use std::ops::Add;
 use std::process::exit;
 use std::time::{Duration, SystemTime};
-
+use std::{env, fs};
 
 /// Steam Api mapping
 #[derive(Deserialize, Debug)]
@@ -33,6 +34,10 @@ struct WorkshopContent {
     publishedfileid: String,
     time_updated: u64,
     preview_url: String,
+
+    // HACK for changelogs, written manually in a different call
+    #[serde(skip_deserializing)]
+    changelog: String,
 }
 ///
 
@@ -42,13 +47,13 @@ struct DiscordWebhook<'a> {
     username: &'a str,
     avatar_url: &'a str,
     content: &'a str,
-    embeds: Vec<DiscordEmbeds>
+    embeds: Vec<DiscordEmbeds>,
 }
 
 #[derive(Serialize, Debug)]
 struct DiscordEmbeds {
     title: String,
-    // description: String,
+    description: String,
     color: String,
     thumbnail: DiscordEmbedThumbnail,
     url: String,
@@ -60,8 +65,23 @@ struct DiscordEmbedThumbnail {
 }
 ///
 
+/// Arguments
+#[derive(ClapParser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, env("STEAM_WEB_KEY"), help("Steam web api key to do calls with"))]
+    steam_key: String,
 
+    #[arg(long, env("DISCORD_WEB_KEY"), requires("user_agent"), help("Pass in a discord webhook to enable discord support, you must also set header_info. Only pass in everything after 'https://discord.com/api/webhooks/'"))]
+    discord_key: Option<String>,
 
+    #[arg(long, env("ACF"), help("Points to a workshop .acf file"))]
+    acf_file: String,
+
+    #[arg(long, env("USER_AGENT"), help("Required if using discord for changelog support, format it such as `Wrench - email@email.com` so people can reach out if need be"))]
+    user_agent: Option<String>,
+}
+///
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -69,20 +89,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // not worth cluttering up the terminal
     let _ = color_eyre::install();
 
-    // Required variables
-    let key = env::var("STEAM_WEB_KEY").expect("Missing steam web api key");
-    let path = env::var("ACF").expect("Missing ACF file path");
+    let args = Args::parse();
 
-    // Optional
-    let discord_key = env::var("DISCORD_WEB_HOOK");
-
-    let acf_file = fs::read_to_string(path).unwrap();
-    let acf: AcfValue = parser::file_to_acf(&acf_file).expect("Could not parse the acf file provided");
+    let acf_file = fs::read_to_string(args.acf_file).unwrap();
+    let acf: AcfValue =
+        parser::file_to_acf(&acf_file).expect("Could not parse the acf file provided");
 
     let workshop: Option<HashMap<&str, u64>> = match acf {
-       AcfValue::Collection(_, body) => {
-            Some(parser::extract_workshop(body))
-        },
+        AcfValue::Collection(_, body) => Some(parser::extract_workshop(body)),
         _ => None,
     };
 
@@ -102,53 +116,77 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         total
     };
 
-    let url = format!("https://api.steampowered.com/IPublishedFileService/GetDetails/v1/?key={}{}&short_description=true&includeforsaledata=false&includemetadata=false&appid=108600&strip_description_bbcode=true", key, all_workshops );
+    let url = format!("https://api.steampowered.com/IPublishedFileService/GetDetails/v1/?key={}{}&short_description=true&includeforsaledata=false&includemetadata=false&appid=108600&strip_description_bbcode=true", args.steam_key, all_workshops);
 
-    let res: SteamApiResponse = reqwest::get(url)
-        .await?
-        .json()
-        .await?;
+    let client = reqwest::ClientBuilder::new().gzip(true);
+    let client = client
+        .build()
+        .expect("Could not build reqwest client, this is bad");
 
+    let res: SteamApiResponse = client.get(url).send().await?.json().await?;
 
-    let mut update_list:  Vec<WorkshopContent> = Vec::new();
+    let mut update_list: Vec<WorkshopContent> = res
+        .response
+        .publishedfiledetails
+        .into_par_iter()
+        .filter(|details| {
+            let timestamp = workshop.get(details.publishedfileid.as_str());
 
-    res.response.publishedfiledetails.into_iter().for_each(|details | {
-        let timestamp = workshop.get(details.publishedfileid.as_str());
+            if timestamp.is_none() {
+                return false;
+            }
 
-        if timestamp.is_none() {
-            return
-        }
+            let timestamp = timestamp.unwrap();
 
-        let timestamp = timestamp.unwrap();
+            if &details.time_updated > timestamp {
+                println!("{:?} needs updating!", details.title);
+                return true;
+            }
 
-        if &details.time_updated > timestamp {
-            println!("{:?} needs updating!", details.title);
-
-            update_list.push(details);
-        }
-    });
+            false
+        })
+        .collect();
 
     if update_list.is_empty() {
         exit(1);
     }
 
     // TODO: Allow people to change the url and username
-    if let Ok(key) = discord_key {
+    if let Some(key) = args.discord_key {
+        println!("Hello world");
         let mut rng = thread_rng();
-        let time = SystemTime::now().add(Duration::from_secs(60 * 5)).duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        let time = SystemTime::now()
+            .add(Duration::from_secs(60 * 5))
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         let content = format!("<bzzt> The server is due to restart at <t:{:?}>, the following mods have been updated: <kssht>", time);
 
-        let embeds: Vec<DiscordEmbeds> = update_list.into_iter().map(|update_list| {
-            DiscordEmbeds{
+        let header = &args.user_agent.unwrap_or_default();
+        for mut update in update_list.iter_mut() {
+            update.changelog = fetch_changelog(&update.publishedfileid, header)
+                .await
+                .unwrap_or_else(|| "No change log could be fetched".to_string());
+        }
+
+        let embeds = update_list
+            .into_iter()
+            .map(|update_list| DiscordEmbeds {
                 title: update_list.title,
                 color: {
                     let n: u16 = rng.gen();
                     n.to_string()
                 },
-                thumbnail: DiscordEmbedThumbnail{ url: update_list.preview_url },
-                url: format!("https://steamcommunity.com/sharedfiles/filedetails/?id={}", update_list.publishedfileid)
-            }
-        }).collect();
+                thumbnail: DiscordEmbedThumbnail {
+                    url: update_list.preview_url,
+                },
+                url: format!(
+                    "https://steamcommunity.com/sharedfiles/filedetails/?id={}",
+                    update_list.publishedfileid
+                ),
+                description: update_list.changelog,
+            })
+            .collect();
 
         let msg: DiscordWebhook = DiscordWebhook {
             avatar_url: "https://i.imgur.com/mXpcHBX.png",
@@ -158,11 +196,88 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         let client = reqwest::Client::new();
-        client.post(format!("https://discord.com/api/webhooks/{}", key))
+        client
+            .post(format!("https://discord.com/api/webhooks/{}", key))
             .json(&msg)
             .send()
             .await?;
     }
 
     Ok(())
+}
+
+async fn fetch_changelog(id: &String, header: &String) -> Option<String> {
+    let client = reqwest::ClientBuilder::new();
+    let client = client.gzip(true).build();
+
+    if let Err(why) = client {
+        println!("Could not build reqwest client for changelogs, {}", why);
+        return None;
+    }
+
+    let req = client
+        .unwrap()
+        .get(format!(
+            "https://steamcommunity.com/sharedfiles/filedetails/changelog/{}",
+            id
+        ))
+        .header(USER_AGENT, header)
+        .send()
+        .await;
+
+    if let Err(why) = req {
+        println!("Could not do changelog request, {}", why);
+        return None;
+    }
+
+    let text = req
+        .unwrap()
+        .text()
+        .await
+        .expect("Could not convert text to utf8");
+
+    let text = text.as_str();
+
+    if text.len() > u32::MAX as usize {
+        println!(
+            "Text body bigger then {}, body was {}. Can not be parsed by tl.",
+            u32::MAX,
+            text
+        );
+        return None;
+    }
+
+    let dom = tl::parse(text, tl::ParserOptions::default()).unwrap();
+
+    let parser = dom.parser();
+    let latest_cl = dom
+        .query_selector(".workshopAnnouncement")
+        .and_then(|mut iter| iter.next());
+
+    if latest_cl.is_none() {
+        println!(
+            "Could not find .workshopAnnouncement for id {}, most likely no change log provided",
+            id
+        );
+        return None;
+    }
+
+    let content = latest_cl.unwrap().get(parser).unwrap();
+    let p = content
+        .as_tag()
+        .unwrap()
+        .query_selector(parser, "p")
+        .and_then(|mut iter| iter.next());
+
+    if p.is_none() {
+        println!(
+            "Could not find p tag for id {}, most likely no change log provided",
+            id
+        );
+        return None;
+    }
+
+    let text = p.unwrap().get(parser).unwrap().inner_html(parser);
+
+    Some(text.replace("<br></br>", "\n"))
 }
