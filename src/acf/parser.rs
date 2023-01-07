0e82961c7dfb21d607702dae4d0adcaf1bfd6925
Vamist <dm@vamist.dev>
2023-01-07 14:14:31 +0000

    [fix] Clippy on parser, wrapped box around error due to size

--- src/acf/parser.rs
+++ src/acf/parser.rs
@@ -1,14 +1,7 @@
-use rand::{thread_rng, Rng};
-
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::{env, fs};
-use std::ops::Add;
-use std::process::exit;
-use std::time::Duration;
-use pest::Parser;
-use pest::error::Error;
 
+use pest::error::Error;
+use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "./acf/acf.pest"]
@@ -18,13 +11,15 @@ pub struct AcfParser;
 pub enum AcfValue<'a> {
     Pair(&'a str, &'a str),
     Collection(&'a str, Vec<AcfValue<'a>>),
-    Null
+    Null,
 }
 
 // Read's a file and attempts to parse it into an AcfValue
-pub fn file_to_acf(file: &str) -> Result<AcfValue, Error<Rule>> {
+pub fn file_to_acf(file: &str) -> Result<AcfValue, Box<Error<Rule>>> {
     // Just panic here if the file is invalid
-    let acf = AcfParser::parse(Rule::collection, file)?.next().expect("Acf file provided could not be read");
+    let acf = AcfParser::parse(Rule::collection, file)?
+        .next()
+        .expect("Acf file provided could not be read");
 
     use pest::iterators::Pair;
     fn parse_value(pair: Pair<Rule>) -> AcfValue {
@@ -34,28 +29,17 @@ pub fn file_to_acf(file: &str) -> Result<AcfValue, Error<Rule>> {
                 let name = pair.next().unwrap().as_str().trim_matches('\"');
                 let result = pair.next().unwrap().as_str().trim_matches('\"');
 
-
-                AcfValue::Pair(
-                    name,
-                    result,
-                )
-            },
+                AcfValue::Pair(name, result)
+            }
             Rule::collection => {
                 let mut inner_rules = pair.into_inner();
                 let name = inner_rules.next().unwrap().as_str().trim_matches('\"');
 
-                let contents = inner_rules
-                    .map(|rule| {
-                        parse_value(rule)
-                    })
-                    .collect();
+                let contents = inner_rules.map(parse_value).collect();
 
-                AcfValue::Collection(
-                    name,
-                    contents
-                )
-            },
-            _ => AcfValue::Null
+                AcfValue::Collection(name, contents)
+            }
+            _ => AcfValue::Null,
         }
     }
 
@@ -86,13 +70,8 @@ pub fn extract_workshop(content: Vec<AcfValue>) -> HashMap<&str, u64> {
                     });
 
                     if let Some(AcfValue::Pair(_, value)) = result {
-                        workshops.insert(
-                            name,
-                            value.parse::<u64>().unwrap()
-                        );
+                        workshops.insert(name, value.parse::<u64>().unwrap());
                     }
-
-
                 }
             })
         }
